8aa46c0c5383001616dc0210b7145b9dae042568
guillevs <guille.vs.93@gmail.com>
2015-05-04 22:31:39 +0200

    Despliegue en Heroku + créditos //rehacer

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 var quizController = require ('../controllers/quiz_controller');
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+
+router.get('/author', function(req, res) {res.render('author', { title: 'Creditos' });
+});
+
 
 module.exports = router;
