4e0cb3a081138f55299037eb4d7f5df5a5c6852f
guillevs <guille.vs.93@gmail.com>
2015-05-04 19:34:59 +0200

    Primera página+esqueleto express+ no favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz1' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
